788c73b171fc1e6fa10c77a96b64636ca1223c49
Colins <77582749+ColinUnity@users.noreply.github.com>
2021-08-10 21:47:08 -0400

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,8 @@
+animals = ["Chickens", "Cows", "Cats", "Dogs"] #creats list under variable
+           #0  -4       1 -3    2 -1    3 -1
+print(animals) #prints the entire list
+print(animals[0]) #prints the first item on the list
+print(animals[-1]) #prints the last item on the list
+print(animals[0:3]) #prints a range of the first item to the third
+animals[1] ="Seahorse" #changes the 2nd item to another name
+print(animals[1]) #prints new item
